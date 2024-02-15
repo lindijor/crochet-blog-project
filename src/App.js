271d45f5ddc3439979914ff70d2f396b5946360b
lindijor <jorgensenlindis@gmail.com>
2024-02-15 14:23:29 +0100

    Added and styled main button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ export default function App() {
   return (
     <div>
       <h1>Velkommen til heklebloggen</h1>
+      <a href="/" className="btn">
+        Se blogginnlegg
+      </a>
     </div>
   );
 }
