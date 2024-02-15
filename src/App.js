99381f103942863f068a4117d84b31919d4d63c6
lindijor <jorgensenlindis@gmail.com>
2024-02-15 14:46:23 +0100

    Added image as background for header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
+import crochet from "./crochet.jpg";
 
 export default function App() {
+  const bakgrunnsbilde = {
+    backgroundImage: `url(${crochet})`,
+    height: `100vh`,
+    backgroundSize: `cover`,
+    backgroundPosition: `center`,
+    backgroundRepeat: `no-repeat`,
+  };
+
   return (
     <div>
-      <h1>Velkommen til heklebloggen</h1>
-      <a href="/" className="btn">
-        Se blogginnlegg
-      </a>
+      <div style={bakgrunnsbilde}>
+        <h1>Velkommen til heklebloggen</h1>
+        <a href="/" className="btn">
+          Se blogginnlegg
+        </a>
+      </div>
     </div>
   );
 }
