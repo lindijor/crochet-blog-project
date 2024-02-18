08b6dae83779fbe292625f72ff3f9d044a1efd46
lindijor <jorgensenlindis@gmail.com>
2024-02-18 22:07:22 +0100

    Created image gallery page

--- src/Bildegalleri.js
+++ src/Bildegalleri.js
@@ -1,9 +1,28 @@
 import React from "react";
+import "./Gallery.css";
+import crochet from "./crochet.jpg";
 
 export default function Bildegalleri() {
   return (
-    <div>
-      <h1>Hello from bildergalleri</h1>
+    <div className="gallery">
+      <h2>Bildegalleri</h2>
+      <div className="gallery-grid">
+        <img src={crochet} alt="crochet" />
+        <img src={crochet} alt="crochet" />
+        <img src={crochet} alt="crochet" />
+        <img src={crochet} alt="crochet" />
+        <img src={crochet} alt="crochet" />
+        <img src={crochet} alt="crochet" />
+        <img src={crochet} alt="crochet" />
+        <img src={crochet} alt="crochet" />
+        <img src={crochet} alt="crochet" />
+        <img src={crochet} alt="crochet" />
+        <img src={crochet} alt="crochet" />
+        <img src={crochet} alt="crochet" />
+      </div>
+      <a href="/" className="btn">
+        Se mer i blogginnlegg
+      </a>
     </div>
   );
 }
