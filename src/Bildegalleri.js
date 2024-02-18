7717ed8cd72c338b46042634a9ab2fee4a3ce5b6
lindijor <jorgensenlindis@gmail.com>
2024-02-18 22:30:41 +0100

    Adjustments to the styling of image gallery

--- src/Bildegalleri.js
+++ src/Bildegalleri.js
@@ -5,23 +5,59 @@ import crochet from "./crochet.jpg";
 export default function Bildegalleri() {
   return (
     <div className="gallery">
-      <h2>Bildegalleri</h2>
+      <h1>Bildegalleri</h1>
       <div className="gallery-grid">
-        <img src={crochet} alt="crochet" />
-        <img src={crochet} alt="crochet" />
-        <img src={crochet} alt="crochet" />
-        <img src={crochet} alt="crochet" />
-        <img src={crochet} alt="crochet" />
-        <img src={crochet} alt="crochet" />
-        <img src={crochet} alt="crochet" />
-        <img src={crochet} alt="crochet" />
-        <img src={crochet} alt="crochet" />
-        <img src={crochet} alt="crochet" />
-        <img src={crochet} alt="crochet" />
-        <img src={crochet} alt="crochet" />
+        <div>
+          <img src={crochet} alt="crochet" />
+          <p>Kort beskrivelse av bildet</p>
+        </div>
+        <div>
+          <img src={crochet} alt="crochet" />
+          <p>Kort beskrivelse av bildet</p>
+        </div>
+        <div>
+          <img src={crochet} alt="crochet" />
+          <p>Kort beskrivelse av bildet</p>
+        </div>
+        <div>
+          <img src={crochet} alt="crochet" />
+          <p>Kort beskrivelse av bildet</p>
+        </div>
+        <div>
+          <img src={crochet} alt="crochet" />
+          <p>Kort beskrivelse av bildet</p>
+        </div>
+        <div>
+          <img src={crochet} alt="crochet" />
+          <p>Kort beskrivelse av bildet</p>
+        </div>
+        <div>
+          <img src={crochet} alt="crochet" />
+          <p>Kort beskrivelse av bildet</p>
+        </div>
+        <div>
+          <img src={crochet} alt="crochet" />
+          <p>Kort beskrivelse av bildet</p>
+        </div>
+        <div>
+          <img src={crochet} alt="crochet" />
+          <p>Kort beskrivelse av bildet</p>
+        </div>
+        <div>
+          <img src={crochet} alt="crochet" />
+          <p>Kort beskrivelse av bildet</p>
+        </div>
+        <div>
+          <img src={crochet} alt="crochet" />
+          <p>Kort beskrivelse av bildet</p>
+        </div>
+        <div>
+          <img src={crochet} alt="crochet" />
+          <p>Kort beskrivelse av bildet</p>
+        </div>
       </div>
       <a href="/" className="btn">
-        Se mer i blogginnlegg
+        Se mer hekling i blogginnlegg
       </a>
     </div>
   );
--- src/Kontakt.js
+++ src/Kontakt.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Kontakt() {
   return (
-    <div>
+    <div className="kontakt">
       <h1>Hello from kontakt</h1>
     </div>
   );
