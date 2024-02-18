f1da7ceb81bf193226f23cc03987542933f630a1
lindijor <jorgensenlindis@gmail.com>
2024-02-18 22:24:20 +0100

    Created page for blog posts

--- src/Blogginnlegg.js
+++ src/Blogginnlegg.js
@@ -1,9 +1,93 @@
 import React from "react";
+import crochet from "./crochet.jpg";
+import "./Blogginnlegg.css";
 
 export default function Blogginnlegg() {
   return (
-    <div>
-      <h1>Hello from blogginnlegg</h1>
+    <div className="blogginnlegg">
+      <h1>Blogginnlegg</h1>
+      <div className="blogginnlegg-grid">
+        <div>
+          <img src={crochet} alt="crochet" />
+        </div>
+        <div className="bloggtekst">
+          <h2>Blogginnleggets tittel</h2>
+          <p>
+            Liten kort introtekst om innlegget som ligger her. Dette er et
+            superfint blogginnlegg du burde lese! Ble du nysgjerrig nå? Les mer
+            her da vel. Liten kort introtekst om innlegget som ligger her. Dette
+            er et superfint blogginnlegg du burde lese! Ble du nysgjerrig nå?
+            Les mer her da vel
+          </p>
+          <a href="/" className="btn">
+            Les blogginnlegget
+          </a>
+        </div>
+        <div>
+          <img src={crochet} alt="crochet" />
+        </div>
+        <div className="bloggtekst">
+          <h2>Blogginnleggets tittel</h2>
+          <p>
+            Liten kort introtekst om innlegget som ligger her. Dette er et
+            superfint blogginnlegg du burde lese! Ble du nysgjerrig nå? Les mer
+            her da vel. Liten kort introtekst om innlegget som ligger her. Dette
+            er et superfint blogginnlegg du burde lese! Ble du nysgjerrig nå?
+            Les mer her da vel
+          </p>
+          <a href="/" className="btn">
+            Les blogginnlegget
+          </a>
+        </div>
+        <div>
+          <img src={crochet} alt="crochet" />
+        </div>
+        <div className="bloggtekst">
+          <h2>Blogginnleggets tittel</h2>
+          <p>
+            Liten kort introtekst om innlegget som ligger her. Dette er et
+            superfint blogginnlegg du burde lese! Ble du nysgjerrig nå? Les mer
+            her da vel. Liten kort introtekst om innlegget som ligger her. Dette
+            er et superfint blogginnlegg du burde lese! Ble du nysgjerrig nå?
+            Les mer her da vel
+          </p>
+          <a href="/" className="btn">
+            Les blogginnlegget
+          </a>
+        </div>
+        <div>
+          <img src={crochet} alt="crochet" />
+        </div>
+        <div className="bloggtekst">
+          <h2>Blogginnleggets tittel</h2>
+          <p>
+            Liten kort introtekst om innlegget som ligger her. Dette er et
+            superfint blogginnlegg du burde lese! Ble du nysgjerrig nå? Les mer
+            her da vel. Liten kort introtekst om innlegget som ligger her. Dette
+            er et superfint blogginnlegg du burde lese! Ble du nysgjerrig nå?
+            Les mer her da vel
+          </p>
+          <a href="/" className="btn">
+            Les blogginnlegget
+          </a>
+        </div>
+        <div>
+          <img src={crochet} alt="crochet" />
+        </div>
+        <div className="bloggtekst">
+          <h2>Blogginnleggets tittel</h2>
+          <p>
+            Liten kort introtekst om innlegget som ligger her. Dette er et
+            superfint blogginnlegg du burde lese! Ble du nysgjerrig nå? Les mer
+            her da vel. Liten kort introtekst om innlegget som ligger her. Dette
+            er et superfint blogginnlegg du burde lese! Ble du nysgjerrig nå?
+            Les mer her da vel
+          </p>
+          <a href="/" className="btn">
+            Les blogginnlegget
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
