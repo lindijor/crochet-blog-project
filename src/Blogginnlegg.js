25c6b8925b06929ef5d67211202a943de253d161
lindijor <jorgensenlindis@gmail.com>
2024-02-18 23:22:30 +0100

    Adjustments to the blog page

--- src/Blogginnlegg.js
+++ src/Blogginnlegg.js
@@ -6,6 +6,13 @@ export default function Blogginnlegg() {
   return (
     <div className="blogginnlegg">
       <h1>Blogginnlegg</h1>
+      <p className="blogg-intro">
+        Kort introtekst om innleggene man kan finne og lese om her. Dette er et
+        superfint blogginnlegg du burde lese! Ble du nysgjerrig nå? Les mer her
+        da vel. Liten kort introtekst om innlegget som ligger her. Dette er et
+        superfint blogginnlegg du burde lese! Ble du nysgjerrig nå? Les mer her
+        da vel
+      </p>
       <div className="blogginnlegg-grid">
         <div>
           <img src={crochet} alt="crochet" />
